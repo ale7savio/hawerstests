c094052e7a5eae73f20d13b8cd27f124e7f1f9ba
Alejandro <alejandro.munoz@saldum.com>
2020-01-15 15:28:04 +0100

    Prueba

--- shopify-orders-tests/src/main/java/com/kafka/tests/shopifyorderstests/controllers/TestsOrdersShopifyController.java
+++ shopify-orders-tests/src/main/java/com/kafka/tests/shopifyorderstests/controllers/TestsOrdersShopifyController.java
@@ -1,6 +1,11 @@
 package com.kafka.tests.shopifyorderstests.controllers;
 
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,19 +22,54 @@ public class TestsOrdersShopifyController {
 	private static final Logger logger = LogManager.getLogger(TestsOrdersShopifyController.class);
 
 	@RequestMapping("/send-order")
-	public String sendOrder(@RequestHeader Map<String, String> headers) {
+	public String sendOrder(@RequestHeader Map<String, String> headers, HttpServletRequest req) {
 		
-	    headers.forEach((key, value) -> {
-	    	logger.debug(String.format("Header '%s' = %s", key, value));
-	    });
+//	    headers.forEach((key, value) -> {
+//	    	logger.debug(String.format("Header '%s' = %s", key, value));
+//	    });
+	    
+	    
 		    
 		//String body = request.getBody();
 		//String hmac = request.getHeader("HTTP_X_SHOPIFY_HMAC_SHA256");
+	    logger.debug(req);
+	    logger.debug(req.getHeader("HTTP_X_SHOPIFY_HMAC_SHA256"));
 
-		System.out.println("Pedido:");
+		System.out.println("Separacion:");
+		
+		getRequestInformation(req);
+		
+		
 		// System.out.println(order);
 		//logger.debug(order);
 		return "header";
+	}
+	
+	private void getRequestInformation(HttpServletRequest request) {
+	    Map<String, String> map = new HashMap<String, String>();
+	    Enumeration headerNames = request.getHeaderNames();
+	    while (headerNames.hasMoreElements()) {
+	        String key = (String) headerNames.nextElement();
+	        String value = request.getHeader(key);
+	        map.put("header: " + key, value);
+	        logger.debug(String.format("1 Header '%s' = %s", key, value));
+	    }
+	    Enumeration parameterNames = request.getParameterNames();
+	    while (parameterNames.hasMoreElements()) {
+	        String key = (String) parameterNames.nextElement();
+	        String value = request.getParameter(key);
+	        map.put("parameter: " + key, value);
+	    }
+	    
+	    while (parameterNames.hasMoreElements()) {
+	        String key = (String) parameterNames.nextElement();
+	        String value = request.getParameter(key);
+	        map.put("parameter: " + key, value);
+	        logger.debug(String.format("2 Header '%s' = %s", key, value));
+	    }
+	    
+	    
+	    System.out.println(map.toString());
 	}
 
 }
